50bad342b369704c9f71b47c34d70995a2845d9f
zpaufdem <zpaufdem@mtu.edu>
2020-08-19 18:12:52 -0400

    Update test.js

--- test.js
+++ test.js
@@ -5,11 +5,12 @@ console.log(data);
     console.log(value);
     
     var city = value.name;
-    var temp = value.main;
+   // var temp = value.main;
     //var city = "test city"
     var temp = "9001"
     
-   // $('.city').text(city);
-   // $('.temp').text(temp);
+   $('.city').text(city);
+   $('.temp').text(temp);
+   $('.image img'.attr('src', "https://media.apnarm.net.au/media/images/2018/10/24/imagev19ad7d458a44d1af8f6dfeda0f1fd0eb6-67gwwj38cojd07c86r2_t1880.jpg");
   });  
 });
